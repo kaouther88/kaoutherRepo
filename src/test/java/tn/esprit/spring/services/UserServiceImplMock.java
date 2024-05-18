34a22b4cbac9a8ef515338269622ead180c441f9
unknown <khalil.kaouther@esprit.tn>
2024-05-18 14:28:47 +0100

    first commit

--- src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplMock.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+//Hello
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
